fc71693eed58bba4e0035066f3b6c86aef12fadd
David J. M. Karlsen <david.johan.macdonald.karlsen@dnb.no>
2026-02-23 12:47:19 +0100

    switch observability libs

--- src/main/java/com/davidkarlsen/App.java
+++ src/main/java/com/davidkarlsen/App.java
@@ -1,9 +1,10 @@
 package com.davidkarlsen;
 
+import org.apache.camel.micrometer.observability.starter.CamelMicrometerObservability;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@CamelMicrometerObservability
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
